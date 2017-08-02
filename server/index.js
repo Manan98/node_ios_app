31384415ba3bcb2318b32856c33967e3f84d6dc0
Manan98 <manan.t.chugh@gmail.com>
2017-08-02 08:14:36 +0530

    server sends email-forgot password

--- server/index.js
+++ server/index.js
@@ -1,18 +1,43 @@
 var http = require('http'),
     express = require('express'),
     MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
+    Server = require('mongodb').Server,
+    nodemailer = require('nodemailer');
 
 var app=express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
 app.use(bodyParser.json());
+
+app.post('/forgot_password', function (req, res) {
+  var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '...@gmail.com',
+    pass: ''
+  }
+});
+
+var mailOptions = {
+  from: '...@gmail.com',
+  to: req.body.email,
+  subject: 'Your new password',
+  text: 'Hi, \n \nThis is your new temporary password: ...'+ 
+  'Please login with this password and then change it in your '+
+  'profile section in the app.\n \nThank you.'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+})
+
 app.post('/login', function (req, res) {
   console.log(req.body.email)
   console.log(req.body.password)
